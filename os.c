3b693d7fb52ed4bc64d6d935a19a303183a71572
Aman Gupta <35022615+er-aman-gupta@users.noreply.github.com>
2018-04-08 16:07:15 +0530

    Did some enhancements.
    Now shows gantt chart and did some fixes/

--- os.c
+++ os.c
@@ -1,8 +1,21 @@
+#include<windows.h>
+#include<conio.h>
 #include<stdio.h>
 #include<windows.h>
 #include<stdlib.h>
 #include<malloc.h>
+COORD coord={0,0}; // this is global variable
+                                    //center of axis is set to the top left cornor of the screen
+ void gotoxy(int x,int y)
+ {
+   coord.X=x;
+ coord.Y=y;
+ SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
+ }
 int total_running_time=0;
+int a[100],acount;
+acount=0;
+char out[1000],temp1[20],temp2[20];
 struct process{
     int pid,burst_time,priority,waiting_time,last_ran,burst1;
     struct process* next;
@@ -48,7 +61,7 @@ void priority_inserter(int pid,int burst,int p)
 
 void inserter()
 {
-
+    strcat(out,"|");
     int ch=1;
     int pid,priority,burst;
     while(ch)
@@ -58,10 +71,20 @@ void inserter()
         scanf("%d",&pid);
         printf("\nEnter Burst Time for the process ");
         scanf("%d",&burst);
+        if(burst<=0)
+        {
+            printf("The burst time cannot be less than 0\nPlease enter again");
+            Sleep(2000);
+            continue;
+        }
         printf("\nEnter priority of the process ");
         scanf("%d",&priority);
-        if(priority>16)
-        {printf("You have entered priority higher than the range!!!\nPlease enter again\n");Sleep(2000);continue;}
+        if(priority<0||priority>16)
+        {
+            printf("You have entered priority higher than the range!!!\nPlease enter again\n");
+            Sleep(2000);
+            continue;
+        }
         if(priority>=6 &&priority<=10)
             priority_inserter(pid,burst,priority);
         else{
@@ -167,11 +190,17 @@ void round_robin()
             //printf("Current time is %d,last ran on %d\n",total_running_time,ptr->last_ran);
             ptr->waiting_time+=total_running_time-ptr->last_ran;
             printf("%d\t\t\tP%d\t\t\t",total_running_time,ptr->pid);
+            sprintf(temp1,"    %d   | ",ptr->pid);
+            strcat(out,temp1);
+            a[acount]=total_running_time;
+            acount++;
             //printf("Process with pid %d is completed\n",ptr->pid);
             total_time-=ptr->burst_time;
             total_running_time+=ptr->burst_time;
             ptr->last_ran=total_running_time;
             printf("%d\n",total_running_time);
+            a[acount]=total_running_time;
+            acount++;
             ptr->burst_time=0;
             if(ptr->next==NULL)
                 ptr=head1;
@@ -183,12 +212,18 @@ void round_robin()
             //printf("Current time is %d,last ran on %d\n",total_running_time,ptr->last_ran);
             ptr->waiting_time+=total_running_time-ptr->last_ran;
             printf("%d\t\t\tP%d\t\t\t",total_running_time,ptr->pid);
+            sprintf(temp1,"    %d   | ",ptr->pid);
+            strcat(out,temp1);
+            a[acount]=total_running_time;
+            acount++;
             //printf("Process with pid %d executed for 4 units of time \n",ptr->pid);
             total_time-=tq;
             ptr->burst_time-=tq;
             total_running_time+=4;
             ptr->last_ran=total_running_time;
             printf("%d\n",total_running_time);
+            a[acount]=total_running_time;
+            acount++;
             if(ptr->next==NULL)
                 ptr=head1;
             else
@@ -198,11 +233,16 @@ void round_robin()
             //printf("Current time is %d,last ran on %d\n",total_running_time,ptr->last_ran);
             ptr->waiting_time+=total_running_time-ptr->last_ran;
             printf("%d\t\t\tP%d\t\t\t",total_running_time,ptr->pid);
-            //printf("Process with pid %d executed for %d units of time \n",ptr->pid,total_time);
+            sprintf(temp1,"    %d   | ",ptr->pid);
+            strcat(out,temp1);
+            a[acount]=total_running_time;
+            acount++;
             ptr->burst_time-=total_time;
             total_running_time+=total_time;
             ptr->last_ran=total_running_time;
             printf("%d\n",total_running_time);
+            a[acount]=total_running_time;
+            acount++;
             total_time=0;
         }
 
@@ -234,11 +274,16 @@ void priority()
       //      printf("Current time is %d,last ran on %d\n",total_running_time,ptr->last_ran);
             ptr->waiting_time+=total_running_time-ptr->last_ran;
             printf("%d\t\t\tP%d\t\t\t",total_running_time,ptr->pid);
-        //    printf("Process with pid %d is completed\n",ptr->pid);
+            sprintf(temp1,"    %d   | ",ptr->pid);
+            strcat(out,temp1);
+            a[acount]=total_running_time;
+            acount++;
             total_time-=ptr->burst_time;
             total_running_time+=ptr->burst_time;
             ptr->last_ran=total_running_time;
             printf("%d\n",total_running_time);
+            a[acount]=total_running_time;
+            acount++;
             ptr->burst_time=0;
             if(ptr->next==NULL)
                 ptr=head2;
@@ -249,11 +294,16 @@ void priority()
           //  printf("Current time is %d,last ran on %d\n",total_running_time,ptr->last_ran);
             ptr->waiting_time+=total_running_time-ptr->last_ran;
             printf("%d\t\t\tP%d\t\t\t",total_running_time,ptr->pid);
-            //printf("Process with pid %d executed for %d units of time \n",ptr->pid,total_time);
+            sprintf(temp1,"    %d   | ",ptr->pid);
+            strcat(out,temp1);
+            a[acount]=total_running_time;
+            acount++;
             ptr->burst_time-=total_time;
             total_running_time+=total_time;
             ptr->last_ran=total_running_time;
             printf("%d\n",total_running_time);
+            a[acount]=total_running_time;
+            acount++;
             total_time=0;
         }
     }
@@ -284,11 +334,16 @@ void fcfs()
       //      printf("Current time is %d,last ran on %d\n",total_running_time,ptr->last_ran);
             ptr->waiting_time+=total_running_time-ptr->last_ran;
             printf("%d\t\t\tP%d\t\t\t",total_running_time,ptr->pid);
-        //    printf("Process with pid %d is completed\n",ptr->pid);
+            sprintf(temp1,"    %d   | ",ptr->pid);
+            strcat(out,temp1);
+            a[acount]=total_running_time;
+            acount++;
             total_time-=ptr->burst_time;
             total_running_time+=ptr->burst_time;
             ptr->last_ran=total_running_time;
             printf("%d\n",total_running_time);
+            a[acount]=total_running_time;
+            acount++;
             ptr->burst_time=0;
             if(ptr->next==NULL)
                 ptr=head3;
@@ -299,11 +354,16 @@ void fcfs()
           //  printf("Current time is %d,last ran on %d\n",total_running_time,ptr->last_ran);
             ptr->waiting_time+=total_running_time-ptr->last_ran;
             printf("%d\t\t\tP%d\t\t\t",total_running_time,ptr->pid);
-            //printf("Process with pid %d executed for %d units of timfe \n",ptr->pid,total_time);
+            sprintf(temp1,"    %d   | ",ptr->pid);
+            strcat(out,temp1);
+            a[acount]=total_running_time;
+            acount++;
             ptr->burst_time-=total_time;
             total_running_time+=total_time;
             ptr->last_ran=total_running_time;
             printf("%d\n",total_running_time);
+            a[acount]=total_running_time;
+            acount++;
             total_time=0;
         }
     }
@@ -318,6 +378,7 @@ void fcfs()
         ptr=ptr->next;
     }
 }*/
+
 void total_waiting_time_calculator()
 {
     int time=0,turn_around=0;
@@ -326,7 +387,6 @@ void total_waiting_time_calculator()
     {
         time+=ptr->waiting_time;
         turn_around+=ptr->burst1+ptr->waiting_time;
-        //printf("Process id is %d waiting time is %d\n",ptr->pid,ptr->waiting_time);
         ptr=ptr->next;
     }
     ptr=head2;
@@ -334,7 +394,6 @@ void total_waiting_time_calculator()
     {
         time+=ptr->waiting_time;
         turn_around+=ptr->burst1+ptr->waiting_time;
-        //printf("Process id is %d waiting time is %d\n",ptr->pid,ptr->waiting_time);
         ptr=ptr->next;
     }
     ptr=head3;
@@ -342,7 +401,6 @@ void total_waiting_time_calculator()
     {
         time+=ptr->waiting_time;
         turn_around+=ptr->burst1+ptr->waiting_time;
-        //printf("Process id is %d waiting time is %d\n",ptr->pid,ptr->waiting_time);
         ptr=ptr->next;
     }
     printf("\nThe Total waiting time is %d\nTotal Turn around time is %d\n",time,turn_around);
@@ -350,6 +408,38 @@ void total_waiting_time_calculator()
     avg_time=(float)time/count;
     avg_turn_around=(float)turn_around/count;
     printf("\nAverage waiting time is %f\nAverage turn around time is %f\n",avg_time,avg_turn_around);
+}
+void gantt_printer()
+{
+    int x=0,y=1;
+    int i=0,j=0;
+    int pipe[30];
+    int pipecount=0;
+    for(int i=0;i<strlen(out);i++)
+    {
+        if(isdigit(out[i])||out[i]==124)
+        {
+            gotoxy(x,y);
+            printf("%c",out[i]);
+            pipe[pipecount]=x;
+            pipecount++;
+            x+=4;
+
+        }
+
+    }
+    x=0;
+    y=2;
+    i=0;
+    j=0;
+    for(i=0;i<acount;i+=2)
+    {
+        gotoxy(pipe[j],y);
+        printf("%d",a[i]);
+        j+=2;
+    }
+    gotoxy(pipe[j],y);
+    printf("%d",total_running_time);
 }
 int main()
 {
@@ -361,11 +451,61 @@ int main()
     while(!isExhausted(head1)||!isExhausted(head2)||!isExhausted(head3))
     {
             round_robin();
-    priority();
-    fcfs();
+            priority();
+            fcfs();
     }
     printf("Total time taken=%d\n",total_running_time);
     total_waiting_time_calculator();
+    system("pause");
+    system("cls");
+  //  system("cls");/*
+    //printf("%s\n",out);
+    printf("Gantt Chart");
+    gantt_printer();
+    //printf("%d\n",acount);
+            /*int x=2,y=0;
+            for(j=0;j<strlen(out)-1;j++)
+            {
+                if(out[j+1]==124)
+                {
+                    gotoxy(x,y);
+                    printf("%d",a[i]);
+                    i+=2;
+                    x+=8;
+                }
+                else printf(" ");
+            }*/
+    /*int x=0,y=0;
+    int i=0,j=0;
+    int pipe[30];
+    int pipecount=0;
+    for(int i=0;i<strlen(out);i++)
+    {
+        if(isdigit(out[i])||out[i]==124)
+        {
+            gotoxy(x,y);
+            printf("%c",out[i]);
+            pipe[pipecount]=x;
+            pipecount++;
+            x+=4;
+
+        }
+
+    }
+    x=0;
+    y=1;
+    i=0;
+    j=0;
+    for(i=0;i<acount;i+=2)
+    {
+        gotoxy(pipe[j],y);
+        printf("%d",a[i]);
+        j+=2;
+    }
+    gotoxy(pipe[j],y);
+    printf("%d",total_running_time);
+*/
+
 }
 /*
 1
@@ -393,3 +533,4 @@ int main()
 14
 0
 */
+
